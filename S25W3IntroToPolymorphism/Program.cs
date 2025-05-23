458ed9e3c13e2ebdb46cf22b8c040dbae93bc0c0
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-05-23 08:36:12 -0400

    method overloading

--- S25W3IntroToPolymorphism/Program.cs
+++ S25W3IntroToPolymorphism/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Add(3, 4);
+            Add("h", "w");
+
+            int[] a = { 3, 4, 5 };
+
+            Print(a);
+        }
+
+        static void Print(int[] arr)
+        {
+            // method body
+        }
+
+        //static void Print(params int[] arr)
+        //{
+        //    // method body
+        //}
+
+        static void Add(int n1, int n2)
+        {
+            // method body
+        }
+
+        //static int Add(int n1, int n2)
+        //{
+        //    return n1 + n2;
+        //}
+
+        static void Add(int n1, int n2, int n3)
+        {
+            // method body
+        }
+
+        static void Add(ref int n1, int n2, int n3)
+        {
+            // method body
+        }
+
+        static void Add(string n1, string n2)
+        {
+            // method body
         }
     }
 }
